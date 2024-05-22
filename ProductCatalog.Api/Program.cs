01b377197aca5650cc4b48a4299d8511b19dcdf5
Joel Q <JoelQvist@live.se>
2024-05-22 13:51:54 +0200

    before publish

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -43,11 +43,10 @@ builder.Services.ConfigureRepositories();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 // Middleware
 app.UseCors("AllowAll");
